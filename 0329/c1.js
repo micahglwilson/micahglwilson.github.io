56e516808ff05afa05dd089f2a40677230521d69
micahglwilson <72531609+micahglwilson@users.noreply.github.com>
2024-03-29 01:12:26 -0500

    0329 LCP

--- 0329/c1.js
+++ 0329/c1.js
@@ -3,7 +3,6 @@
 let R = null;
 
 let b1 = document.querySelector( "#b1" );
-let cahrt1 = null;
 
 async function loadd()
 {
@@ -11,29 +10,8 @@ async function loadd()
     let url = `https://juxinglong.github.io/static/data/states.json`;
 
    let r =await fetch(url);
-   let rj = await r.json();
+    let rj = awair r.json();
 
-    let c1 = document.querySelector("#c1");
-    if (chart1 != null)
-    {
-        chart1.destroy();
-    }
-    c1.innerHTML = ``;
-
-    let opts = {
-        type: "pie",
-       
-        data: {
-            labels: rj.map(x => x.st),
-            datasets: [
-                {data:rj.map(x=>x.p), },
-            ],
-        },
-    };
-    chart1 = new Chart(c1, opts);
-
-    R = rj;
-    console.log(rj);
 
     Swal.fire("Load data");
 }
